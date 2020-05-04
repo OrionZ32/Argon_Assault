003ba1ba28073be65af95c185a937069883bd211
Harley Garcia <hagarcia99@outlook.com>
2020-05-04 17:14:04 -0500

    Added player horizontal movement

--- No_Fly_Zone/Assets/Scripts/PlayerShip.cs
+++ No_Fly_Zone/Assets/Scripts/PlayerShip.cs
@@ -5,12 +5,19 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerShip : MonoBehaviour {
 
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 2.5f;
+
     void Start() {
         
     }
 
     void Update() {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos ,-xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
     }
 }
