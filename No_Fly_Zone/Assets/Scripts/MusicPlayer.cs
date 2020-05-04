6b4d4b3e643ef48b40f1ed20686391261956f5a5
Harley Garcia <hagarcia99@outlook.com>
2020-05-04 16:03:09 -0500

    Imported Unity standard assets and stared work with cross platform input manager

---
+++ No_Fly_Zone/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    void Start() {
+        Invoke("LoadFirstScene", 3f);
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void LoadFirstScene() {
+        SceneManager.LoadScene(1);     
+    }
+}
---
+++ No_Fly_Zone/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerShip : MonoBehaviour {
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(horizontalThrow);
+    }
+}
