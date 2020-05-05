8e892017d4af90e01a0245c6f2261fa782cb915d
Harley Garcia <hagarcia99@outlook.com>
2020-05-04 22:29:59 -0500

    Added vertical movement

--- No_Fly_Zone/Assets/Scripts/PlayerShip.cs
+++ No_Fly_Zone/Assets/Scripts/PlayerShip.cs
@@ -5,19 +5,35 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerShip : MonoBehaviour {
 
-    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 4f;
+
     [Tooltip("In m")] [SerializeField] float xRange = 2.5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 2f;
 
     void Start() {
         
     }
 
     void Update() {
+        HorizontalInputController();
+        VerticalInputController();
+    }
+
+    private void HorizontalInputController() {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xOffset = xThrow * speed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos ,-xRange, xRange);
 
         transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private void VerticalInputController() {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
     }
 }
