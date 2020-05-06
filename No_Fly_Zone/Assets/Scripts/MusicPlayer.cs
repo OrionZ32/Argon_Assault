6d8373335ccc57b381325ad65f7043ba0b143c5c
Harley Garcia <hagarcia99@outlook.com>
2020-05-05 21:45:02 -0500

    Refactored project for better work flow

--- No_Fly_Zone/Assets/Scripts/MusicPlayer.cs
+++ No_Fly_Zone/Assets/Scripts/MusicPlayer.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour {
 
     void Start() {
-        Invoke("LoadFirstScene", 3f);
         DontDestroyOnLoad(this.gameObject);
     }
-
-    void LoadFirstScene() {
-        SceneManager.LoadScene(1);     
-    }
+ 
 }
