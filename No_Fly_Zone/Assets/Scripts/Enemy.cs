3cfd7c350081104e50953f5ea10266cdc192b572
Harley Garcia <hagarcia99@outlook.com>
2020-05-07 10:34:27 -0500

    Enemies have explosion fx and explosion fx self destruct over time

--- No_Fly_Zone/Assets/Scripts/Enemy.cs
+++ No_Fly_Zone/Assets/Scripts/Enemy.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject enemyExplosion;
+    [SerializeField] Transform parent;
+
+    void Start() {
+    Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        enemyBoxCollider.isTrigger = false;    
+    }
 
     void OnParticleCollision(GameObject other) {
+
+        GameObject explosionFx = Instantiate(enemyExplosion, transform.position, Quaternion.identity);
+        explosionFx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
